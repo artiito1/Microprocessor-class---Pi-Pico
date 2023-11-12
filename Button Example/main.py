c1a416cf4ac103b0d04936eef1fc429860035ec5
Ahmed Tate <124245558+artiito1@users.noreply.github.com>
2023-11-12 07:23:57 +0300

    main.py

--- Button Example/main.py
+++ Button Example/main.py
@@ -1,8 +1,19 @@
-from machine import Pin 
+# Import necessary modules
+from machine import Pin
 import time
-button = Pin(12, Pin.IN,Pin.PULL_DOWN)
+
+# Define a button on Pin 12 as an input with a pull-down resistor
+button = Pin(12, Pin.IN, Pin.PULL_DOWN)
+
+# Define an LED on Pin 1 as an output
 led = Pin(1, Pin.OUT)
-while True :
+
+# Infinite loop for continuous operation
+while True:
+    # Check if the button is pressed (its value is True)
     if button.value():
+        # Toggle the state of the LED (turn it on or off)
         led.toggle()
+
+        # Pause for 0.5 seconds to debounce the button and control LED blinking speed
         time.sleep(0.5)
